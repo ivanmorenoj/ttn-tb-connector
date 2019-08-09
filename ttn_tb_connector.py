1f2ec9c1eefbff198bbe14c3f142fc4dfeacdfdf
Ivan Moreno <ivan1697@gmail.com>
2019-08-09 11:05:33 -0500

    transport received message

--- ttn_tb_connector.py
+++ ttn_tb_connector.py
@@ -20,27 +20,16 @@ tb_client.connect(TB_HOST, TB_PORT)
 
 tb_client.loop_start()
 
-sensor_data = {'Temperature': 0,'Humidity': 0,'Pressure': 0,'CO': 0,'O3': 0,'SO2': 0,'NO2': 0,'PM1': 0,'PM10': 0,'PM25': 0}
-
-def uplink_callback(msg, client):
-    sensor_data['Temperature']  = msg.payload_fields.Temperature
-    sensor_data['Humidity']     = msg.payload_fields.Humidity
-    sensor_data['Pressure']     = msg.payload_fields.Pressure
-    sensor_data['CO']           = msg.payload_fields.CO
-    sensor_data['O3']           = msg.payload_fields.O3
-    sensor_data['SO2']          = msg.payload_fields.SO2
-    sensor_data['NO2']          = msg.payload_fields.NO2
-    sensor_data['PM1']          = msg.payload_fields.PM1
-    sensor_data['PM10']         = msg.payload_fields.PM10
-    sensor_data['PM25']         = msg.payload_fields.PM25
-    info = tb_client.publish("v1/devices/me/telemetry", json.dumps(sensor_data), 1)
+def uplink_raw_callback(msg, client):
+    j_msg = json.loads(msg.payload.decode("utf-8"))
+    info = tb_client.publish("v1/devices/me/telemetry", json.dumps(j_msg['payload_fields']), 1)
     info.wait_for_publish()
 
 handler = ttn.HandlerClient(TTN_APP_ID, TTN_ACCESS_KEY)
 
 # using mqtt client
 mqtt_client = handler.data()
-mqtt_client.set_uplink_callback(uplink_callback)
+mqtt_client.set_uplink_raw_callback(uplink_raw_callback)
 mqtt_client.connect()
 
 try:
