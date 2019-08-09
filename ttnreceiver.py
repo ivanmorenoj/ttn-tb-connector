75a0c700e63cbc9499ea1a80c11d1a5435f5c162
Ivan Moreno <ivan1697@gmail.com>
2019-08-09 10:56:58 -0500

    parse json in ttn receiver

--- ttnreceiver.py
+++ ttnreceiver.py
@@ -5,25 +5,15 @@ import json
 TTN_APP_ID = "emca"
 TTN_ACCESS_KEY = "ttn-account-v2.Ebyo38DrTI5dynOb28aw0ExjDLYZGGsr4D6yanc9jcs"
 
-sensor_data = {'Temperature': 0,'Humidity': 0,'Pressure': 0,'CO': 0,'O3': 0,'SO2': 0,'NO2': 0,'PM1': 0,'PM10': 0,'PM25': 0}
-
-def uplink_callback(msg, client):
-    sensor_data['Temperature']  = msg.payload_fields.Temperature
-    sensor_data['Humidity']     = msg.payload_fields.Humidity
-    sensor_data['Pressure']     = msg.payload_fields.Pressure
-    sensor_data['CO']           = msg.payload_fields.CO
-    sensor_data['O3']           = msg.payload_fields.O3
-    sensor_data['SO2']          = msg.payload_fields.SO2
-    sensor_data['NO2']          = msg.payload_fields.NO2
-    sensor_data['PM1']          = msg.payload_fields.PM1
-    sensor_data['PM10']         = msg.payload_fields.PM10
-    sensor_data['PM25']         = msg.payload_fields.PM25
+def uplink_raw_callback(msg, client):
+    j_msg = json.loads(msg.payload.decode("utf-8"))
+    print(j_msg['payload_fields'])
 
 handler = ttn.HandlerClient(TTN_APP_ID, TTN_ACCESS_KEY)
 
 # using mqtt client
 mqtt_client = handler.data()
-mqtt_client.set_uplink_callback(uplink_callback)
+mqtt_client.set_uplink_raw_callback(uplink_raw_callback)
 mqtt_client.connect()
 
 try:
